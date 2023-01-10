ebf08d9726704059733725c28d3e5245e804adda
Frank Ballmeyer <frank@ballmeyer.com>
2023-01-10 12:58:33 +0100

    Changed role name to displayName

--- user-role-api/src/main/java/ballware/keycloak/roleapi/model/Role.java
+++ user-role-api/src/main/java/ballware/keycloak/roleapi/model/Role.java
@@ -2,18 +2,18 @@ package ballware.keycloak.roleapi.model;
 
 public class Role {
     private String id;
-    private String name;
+    private String displayName;
 
-    public Role(String id, String name) {
+    public Role(String id, String displayName) {
         this.id = id;
-        this.name = name;
+        this.displayName = displayName;
     }
 
     public String getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getDisplayName() {
+        return displayName;
     }
 }
