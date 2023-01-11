c98dfa12e4acc8e30f7369c7f8a85c3aaf3a1bed
Frank Ballmeyer <frank@ballmeyer.com>
2023-01-11 17:07:19 +0000

    Completed userapi

--- user-role-api/src/main/java/ballware/keycloak/userapi/UserRestProvider.java
+++ user-role-api/src/main/java/ballware/keycloak/userapi/UserRestProvider.java
@@ -1,16 +1,29 @@
 package ballware.keycloak.userapi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Encoded;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.lang.StringUtils;
 import org.jboss.resteasy.annotations.cache.NoCache;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.keycloak.models.KeycloakSession;
@@ -22,6 +35,7 @@ import org.keycloak.services.resources.Cors;
 import org.keycloak.utils.MediaType;
 
 import ballware.keycloak.userapi.model.User;
+import ballware.keycloak.userapi.model.UserClaim;
 
 public class UserRestProvider implements RealmResourceProvider {
     private final KeycloakSession session;
@@ -44,7 +58,7 @@ public class UserRestProvider implements RealmResourceProvider {
 	@Path("{any:.*}")
 	public Response preflight() {
 		HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
-		return Cors.add(request, Response.ok()).auth().preflight().build();
+		return Cors.add(request, Response.ok()).auth().allowedMethods("OPTIONS", "GET", "POST", "DELETE").preflight().build();
 	}
 
     @GET
@@ -52,18 +66,206 @@ public class UserRestProvider implements RealmResourceProvider {
     @NoCache
     @Produces({MediaType.APPLICATION_JSON})
     @Encoded
-    public List<User> getUsers() {
+    public Response getAllUsers(String identifier) {
+        
+        String tenant = assertUserHasTenant();
+        assertUserHasClaim("right", "identity.user.view");
+
+        HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
+
+        return Cors.add(request, Response
+            .ok(session.users().searchForUserStream(session.getContext().getRealm(), "")
+                .filter(r -> r.getAttributes().getOrDefault("tenant", new ArrayList<String>()).contains(tenant))
+                .map(e -> toUserDetail(e))
+                .collect(Collectors.toList()))
+            ).auth().allowedOrigins(this.auth.getToken()).build();
+    }
+
+    @GET
+    @Path("byId")
+    @NoCache
+    @Produces({MediaType.APPLICATION_JSON})
+    @Encoded
+    public Response getUserById(
+        @QueryParam("identifier") String identifier, 
+        @QueryParam("id") String id) {
+        
+        String tenant = assertUserHasTenant();
+        assertUserHasClaim("right", "identity.user.view");
+
+        HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
+
+        UserModel user = session.users().getUserById(session.getContext().getRealm(), id);
+
+        if (user != null && user.getAttributes().getOrDefault("tenant", new ArrayList<String>()).contains(tenant)) {
+            return Cors.add(request, Response
+                .ok(this.toUserDetail(user))
+            ).auth().allowedOrigins(this.auth.getToken()).build();    
+        }
+
+        return Cors.add(request, Response
+            .status(Status.NOT_FOUND)            
+        ).auth().allowedOrigins(this.auth.getToken()).build();
+    }
+
+
+    @GET
+    @Path("new")
+    @NoCache
+    @Produces({MediaType.APPLICATION_JSON})
+    @Encoded
+    public Response getNewUser(String identifier) {
+        
+        assertUserHasTenant();
+        assertUserHasClaim("right", "identity.user.add");
+        
+        HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
+
+        return Cors.add(request, Response
+            .ok(new User(UUID.randomUUID().toString(), "", "", "", null))
+            ).auth().allowedOrigins(this.auth.getToken()).build();
+    }
+
+    @POST
+    @Path("save")
+    @NoCache
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Encoded
+    public Response saveUser(
+        @QueryParam("identifier") String identifier, User user) {
+
+        String tenant = assertUserHasTenant();
+        assertAuthenticatedUser();
+
+        Map<String, List<String>> foldedClaims = new HashMap<String, List<String>>();
+
+        if (user.getUserClaims() != null) {
+            user.getUserClaims().forEach(cl -> {
+                if (!"tenant".equals(cl.getClaimType())) {
+                    if (!foldedClaims.containsKey(cl.getClaimType())) {
+                        foldedClaims.put(cl.getClaimType(), new ArrayList<String>());
+                    }
+
+                    foldedClaims.get(cl.getClaimType()).add(cl.getClaimValue());
+                }
+            });
+        }
+
+        foldedClaims.put("tenant", Arrays.asList(new String[] { tenant }));
+        
+        HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
+
+        UserModel existingUser = session.users().getUserById(session.getContext().getRealm(), user.getId());
+
+        if (existingUser != null && existingUser.getAttributes().getOrDefault("tenant", new ArrayList<String>()).contains(tenant)) {
+            
+            assertUserHasClaim("right", "identity.user.edit");
+
+            existingUser.setFirstName(user.getFirstName());
+            existingUser.setLastName(user.getLastName());
+
+            foldedClaims.forEach((key, value) -> {
+                existingUser.setAttribute(key, value);
+            });
+
+            return Cors.add(request, Response
+                .ok(this.toUserDetail(existingUser))
+            ).auth().allowedOrigins(this.auth.getToken()).build();    
+        } else if (existingUser == null) {
+            assertUserHasClaim("right", "identity.user.add");
+
+            UserModel newUser = session.users().addUser(session.getContext().getRealm(), user.getId(), user.getUserName(), true, true);
+        
+            foldedClaims.forEach((key, value) -> {
+                newUser.setAttribute(key, value);
+            });
+
+            return Cors.add(request, Response
+                .ok(this.toUserDetail(newUser))
+            ).auth().allowedOrigins(this.auth.getToken()).build();   
+        }
+
+        return Cors.add(request, Response
+            .status(Status.NOT_FOUND)            
+        ).auth().allowedOrigins(this.auth.getToken()).build();
+    }
+
+    @DELETE
+    @Path("remove/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @NoCache
+    @Encoded
+    public Response removeRole(
+        @PathParam("id") String id) {
+
+        String tenant = assertUserHasTenant();
+        assertUserHasClaim("right", "identity.user.delete");
+
+        HttpRequest request = session.getContext().getContextObject(HttpRequest.class);
+
+        UserModel existingUser = session.users().getUserById(session.getContext().getRealm(), id);
+
+        if (existingUser != null && existingUser.getAttributes().getOrDefault("tenant", new ArrayList<String>()).contains(tenant)) {
+            
+            session.users().removeUser(session.getContext().getRealm(), existingUser);
+
+            return Cors.add(request, Response
+                .ok(this.toUserDetail(existingUser))
+            ).auth().allowedOrigins(this.auth.getToken()).build();    
+        }
+
+        return Cors.add(request, Response
+            .status(Status.NOT_FOUND)            
+        ).auth().allowedOrigins(this.auth.getToken()).build();
+    }
+
+    private void assertAuthenticatedUser() {
         if (this.auth == null || this.auth.getToken() == null) {
             throw new NotAuthorizedException("Bearer");
         }
+    }
 
-        return session.users().searchForUserStream(session.getContext().getRealm(), "")
-            .map(e -> toUserDetail(e))
-            .collect(Collectors.toList());
+    private String assertUserHasTenant() {
+
+        assertAuthenticatedUser();
+
+        String tenant = (String)this.auth.getToken().getOtherClaims().getOrDefault("tenant", null);
+
+        if (StringUtils.isBlank(tenant)) {
+            throw new ForbiddenException();
+        }
+
+        return tenant;
+    }
+
+    private void assertUserHasClaim(String claimType, String claimValue) {
+
+        assert StringUtils.isNotBlank(claimType);
+        assert StringUtils.isNotBlank(claimValue); 
+
+        assertAuthenticatedUser();
+
+        Object claimValues = this.auth.getToken().getOtherClaims().getOrDefault(claimType, null);
+
+        boolean isList = claimValues instanceof List<?>;
+        boolean hasClaim = isList ? ((List<?>)claimValues).contains(claimValue) : claimValue.equals(claimValues);
+
+        if (!hasClaim) {
+            throw new ForbiddenException();
+        }
     }
 
     private User toUserDetail(UserModel um) {
-        return new User(um.getUsername(), um.getFirstName(), um.getLastName());
 
+        List<UserClaim> claims = new ArrayList<UserClaim>();
+
+        um.getAttributes().entrySet().forEach(entry -> {
+            if (entry.getKey() != "tenant") {
+                entry.getValue().forEach(value -> claims.add(new UserClaim(null, um.getId(), entry.getKey(), value)));
+            }
+        });
+
+        return new User(um.getId(), um.getUsername(), um.getFirstName(), um.getLastName(), claims);
     }
 }
--- user-role-api/src/main/java/ballware/keycloak/userapi/model/User.java
+++ user-role-api/src/main/java/ballware/keycloak/userapi/model/User.java
@@ -1,25 +1,71 @@
 package ballware.keycloak.userapi.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+    private String id;
     private String userName;
     private String firstName;
     private String lastName;
 
-    public User(String userName, String firstName, String lastName) {
+    private List<UserClaim> claims;
+
+    public User() {
+
+    }
+
+    public User(String id, String userName, String firstName, String lastName, List<UserClaim> claims) {
+        this.id = id;
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.claims = claims;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String value) {
+        this.id = value;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
+    public void setFirstName(String value) {
+        this.firstName = value;
+    }
+
     public String getUserName() {
         return userName;
     }
 
+    public void setUserName(String value) {
+        this.userName = value;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String value) {
+        this.lastName = value;
+    }
+
+    @JsonGetter("userClaims")
+    public List<UserClaim> getUserClaims() {
+        return claims;
+    }
+
+    @JsonSetter("userClaims")
+    public void setUserClaims(List<UserClaim> value) {
+        this.claims = value;
     }
 }
